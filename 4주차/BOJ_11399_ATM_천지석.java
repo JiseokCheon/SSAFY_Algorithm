ccd53bb94b6224a86541f2ee0c5125506791efa2
jscheon <81504973+jscheon@users.noreply.github.com>
2022-03-04 20:28:22 +0900

    Add files via upload

---
+++ 4주차/BOJ_11399_ATM_천지석.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            answer += arr[i] * (N - i);
+        }
+
+        System.out.println(answer);
+    }
+}
