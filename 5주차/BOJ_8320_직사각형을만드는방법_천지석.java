09ffa619c23a51183ab0fe6a24387cacada3fe83
jscheon <81504973+jscheon@users.noreply.github.com>
2022-03-08 12:55:22 +0900

    Add files via upload

---
+++ 5주차/BOJ_8320_직사각형을만드는방법_천지석.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
